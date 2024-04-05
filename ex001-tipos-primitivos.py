58e39bc804661c716e5758eb3640d1733e3c1d63
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-04-05 17:57:17 +0000

    clear

--- ex001-tipos-primitivos.py
+++ ex001-tipos-primitivos.py
@@ -1,6 +1,14 @@
-nome = "Marta"
+import os
 
+os.system("clear || cls")
+
+#teste
+
+nome = "Marta"
 idade = 25
+peso = 60.500
+
 
 print(f"Nome: {nome}")
 print(f"Idade: {idade}")
+print(f"Peso: {peso}")    
