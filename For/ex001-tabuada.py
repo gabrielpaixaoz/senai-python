79053ac7810e03d2506cc6a72f8104e40f050c35
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-04-11 19:48:42 +0000

    bora

---
+++ For/ex001-tabuada.py
@@ -0,0 +1,5 @@
+
+numero = int(input("Digite o número para a tabuada: "))
+
+for i in range(1, 11):
+    print(f"{numero} x i = {numero * i}")
---
+++ For/ex003-input5.py
@@ -0,0 +1,7 @@
+soma = 0
+
+for numero in range(1,6):
+    numero = int(input("Digite o número: "))
+    soma = soma + numero
+
+print(f"{soma}")
