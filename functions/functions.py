938268cf0e8ff83eeb387fc72369a4b8ea1bc21b
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-04-30 14:17:01 -0300

    Create functions.py

---
+++ functions/functions.py
@@ -0,0 +1,19 @@
+import os
+
+def logoSenai():
+    os.system("cls || clear")
+    print("=== SENAI ===")
+
+logoSenai()
+nome = input("Digite seu nome: ")
+
+logoSenai()
+idade = int(input("Digite sua idade: "))
+
+logoSenai()
+peso = float(input("Digite seu peso: "))
+
+logoSenai()
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"peso: {peso}")
