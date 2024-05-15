57c2e5bdd5b5fe2bd6b1e53ea637db41dfc8264c
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-05-15 14:59:07 -0300

    Update inss.py

--- atividades/inss.py
+++ atividades/inss.py
@@ -8,10 +8,20 @@ senhaCorreta : str = '123'
 loginUsuario = input("Digite o seu login: ")
 senhaUsuario = input('Digite a senha: ')
 
+def INSS(salario):
+    if salario > 0 and salario < 1101:
+        desconto = salario * 0.075
+        return salario - desconto
+        
+        
+    
 
 if loginCorreto == loginUsuario and senhaUsuario == senhaCorreta:
     print("Login acessado!")
     salario_base = int(input("Digite seu salário: "))
+    salario_inss = INSS(salario_base)
+    print(salario_base)
+    print(salario_inss)
     
 else:
     print("errado")
